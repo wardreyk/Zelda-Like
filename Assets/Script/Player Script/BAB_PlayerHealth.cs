63657b040f4c755217228db0b02e0661f203b617
Wardreyk <b.bohez@rubika-edu.com>
2021-02-16 17:02:17 +0100

    Player Life System

---
+++ Assets/Script/Player Script/BAB_PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BAB_PlayerHealth : MonoBehaviour
+{
+
+    public int health;
+    public int numofHearts;
+    public int maxHealth = 8;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            // Vérifie la vie du personnage
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            // Ajoute ou supprime des coeurs 
+            if(i < numofHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+            if(health >= numofHearts)
+            {
+                health = maxHealth;
+            }
+        }
+    }
+
+}
