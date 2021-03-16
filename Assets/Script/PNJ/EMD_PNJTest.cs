ecbf70db9e991eb83d18386f220035c34ff9cfd7
Emeric Delemer <emericdelemer@gmail.com>
2021-03-16 16:07:21 +0100

    PNJTriggered

--- Assets/Script/PNJ/EMD_PNJTest.cs
+++ Assets/Script/PNJ/EMD_PNJTest.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EMD_PNJTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool IsInRange = false;
+    public bool DialogueOn = false;
+    public GameObject IdlePNJ;
+    public GameObject DialogueManager;
+    private EMD_DialogueManager DialogueManagerScript;
     void Start()
     {
-        
+        IdlePNJ.SetActive(false);
+        DialogueManagerScript = DialogueManager.GetComponent<EMD_DialogueManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if ((Input.GetButtonDown("Interact")) && IsInRange == true && !DialogueManagerScript.DialogueIsActive)
+        {
+            Debug.Log("Interaction");
+            DialogueManagerScript.ActualNPC = this.gameObject;
+            DialogueManagerScript.StartCoroutine("StartDialogue");
+        }
+        else if (DialogueManagerScript.DialogueIsActive == true)
+        {
+            DialogueManagerScript.DialogueIsActive = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsInRange = true;
+            IdlePNJ.SetActive(true);
+            Debug.Log("C'est un bon joueur !");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsInRange = false;
+        IdlePNJ.SetActive(false);
     }
 }
