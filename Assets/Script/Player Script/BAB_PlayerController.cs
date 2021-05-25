08ade4a024540b248dc560708e70c32516c5b143
Wardreyk <b.bohez@rubika-edu.com>
2021-05-25 16:58:24 +0200

    Update : Idle Animation

--- Assets/Script/Player Script/BAB_PlayerController.cs
+++ Assets/Script/Player Script/BAB_PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class BAB_PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
 
     public float moveSpeed = 5f;
+    public bool isMoving;
 
     [SerializeField]
     Transform Player;
@@ -16,11 +18,20 @@ public class BAB_PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Initialisation du déplacement
+        isMoving = false;
+
         // Inputs déplacements
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
 
-
+        // Animation Idle
+        if (Input.GetAxis("Horizontal") == 1 || Input.GetAxis("Horizontal") == -1 || Input.GetAxis("Vertical") == 1 || Input.GetAxis("Vertical") == -1)
+        {
+            animator.SetFloat("LastMoveX", Input.GetAxis("Horizontal"));
+            animator.SetFloat("LastMoveY", Input.GetAxis("Vertical"));
+            isMoving = true;
+        }
     }
 
     private void FixedUpdate()
@@ -28,7 +39,4 @@ public class BAB_PlayerController : MonoBehaviour
         // Movement du personnage
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-
-
- 
 }
