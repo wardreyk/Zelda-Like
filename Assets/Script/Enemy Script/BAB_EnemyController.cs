89f605203020ec6f405afedd55e47dcb852a50ec
wardreyk <b.bohez@rubika-edu.com>
2021-06-08 01:36:47 +0200

    ==>

---
+++ Assets/Script/Enemy Script/BAB_EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAB_EnemyController : MonoBehaviour
+{
+    private Animator animator;
+    private Transform targer;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
